4af03261a664243fef75c814a3ee41374bec4055
dennisce <dennisce@gmail.com>
2020-04-07 19:07:21 -0300

    pasta cliente

--- sites/default/data/drupal/Agendamentos.sql
+++
@@ -1,9 +0,0 @@
---ACCESS=
-SELECT n.title, fv.field_valor_value, fe.field_executou_value
-FROM node n
-
-INNER JOIN field_data_field_valor fv
-ON fv.entity_id = n.nid
-
-INNER JOIN field_data_field_executou fe
-ON fe.entity_id = n.nid
